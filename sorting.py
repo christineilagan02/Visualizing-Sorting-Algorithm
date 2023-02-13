d7bd7b4f6d803242b1922d679639f64f2241781b
unknown <christineilagan02@gmail.com>
2023-02-13 21:46:05 +0800

    implemented the button generate (original code)

--- sorting.py
+++ sorting.py
@@ -9,7 +9,11 @@ import random
 root = Tk()
 root.title('Sorting Algorithm Visualiser')
 root.geometry('900x600+200+80')
-root.config(bg='black')
+root.config(bg='#082A46')
+
+def Generate():
+    print('Selected Algorithm: ' )
+
 
 
 selected_algorithm = StringVar()
@@ -22,7 +26,12 @@ mainlabel.place(x = 0, y = 0)
 algo_menu = ttk.Combobox(root, width = 15, font = ("new roman", 19, "italic bold"), textvariable = selected_algorithm, 
                          values = ['Bubble Sort', 'Merge Sort', 'Quick Sort'])
 algo_menu.place(x = 145, y = 0)
-algo_menu.current(0)     # by default bubble sort
+algo_menu.current(0)      # by default bubble sort
+
+random_generate = Button(root, text = "Generate", bg = '#2DAE9A', font = ("arial", 12, "italic bold"), 
+                         relief = SUNKEN, activebackground = '#05945B', activeforeground = "white", bd = 5, 
+                         width = 10, command = Generate)
+random_generate.place(x = 750, y = 60)
 
 
 
