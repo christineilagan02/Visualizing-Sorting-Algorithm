646842a102e054736917d1ec71d26c6585015353
unknown <christineilagan02@gmail.com>
2023-02-13 21:30:30 +0800

    importing tkinter (original code)

---
+++ sorting.py
@@ -0,0 +1,17 @@
+
+# Final Project
+# Sorting Algorithm Visualiser (title)
+
+from tkinter import *
+from tkinter import ttk
+import random
+
+root = Tk()
+root.title('Sorting Algorithm Visualiser')
+root.geometry('900x600+200+80')
+root.config(bg='black')
+
+
+
+
+root.mainloop()
